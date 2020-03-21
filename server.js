df971a9a0e3777bcbee39f4bdce156e6878c9a6a
ed99win <edwin99@live.ca>
2020-03-21 13:30:11 -0400

    to use nodemon: npm run server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log('Server started!')
   })
