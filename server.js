3534f591888876240501e541272dfcf6e8aa0f56
ed99win <edwin99@live.ca>
2020-03-21 14:45:19 -0400

    ok

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const http = require('http')
+const path = require('path')
 const app = express()
+const server = http.createServer(app)
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log('Server started!')
+//to send production folder in dist
+
+app.use(express.static(__dirname + '/frontend/dist/frontend/'))
+
+app.get('/*', (req, res)=>{
+  res.sendFile(path.join(__dirname))
+})
+
+server.listen(PORT, () => {
+    console.log(`Server started! on port ${PORT}`)
   })
+
+
