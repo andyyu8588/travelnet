133e043c7f3bcb8a83583bb9f9aa7b412d037a4f
ed99win <edwin99@live.ca>
2020-03-25 17:39:43 -0400

    o

--- client-socket.js
+++ client-socket.js
@@ -2,23 +2,26 @@ const socket = io(`http://localhost:3000`)
 const chat = document.getElementById('chatbox')
 const chatmsg = document.getElementById('text')
 const sender = prompt('sender')
-const receiver = prompt('receiver')
+// const receiver = prompt('receiver')
 const text = document.getElementById("sender")
-const text2 = document.getElementById("receiver")
-text2.innerHTML = `${receiver}`
+// const text2 = document.getElementById("receiver")
+// text2.innerHTML = `${receiver}`
 text.innerHTML = `${sender}`
 
-socket.on('chatmessage', (data)=>{
+socket.on('connect', ()=>{
+    socket.emmit('join_room', "room1")
+})
+
+socket.on('message', (data)=>{
     console.log(data)
     $('#1').append(`<li>${data}</li>`)
-    console.log(socket.id)
 })
 
 chat.addEventListener('submit', (e)=>{
    e.preventDefault()
    if(chatmsg.value){
         $('#1').append(`<li>${chatmsg.value}</li>`)
-        socket.emit('chatmessage', chatmsg.value)
+        socket.emit('message', chatmsg.value)
         chatmsg.value = ''  
    }
 })
