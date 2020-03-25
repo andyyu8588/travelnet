f0d7239d0c9ee549899006b1fc3009e8bef655c4
SimonTheWang <simonwg2001@gmail.com>
2020-03-24 22:47:08 -0400

    please acceot

---
+++ profile.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Voyage', {useNewUrlParser: true, useUnifiedTopology: true});
+
+var profileSchema = new mongoose.Schema({
+    name: String,
+    Desired_Country: String,
+    Local_Country: String,
+    Age: Number,
+})
+
+var Profile = mongoose.model('Profile',profileSchema);
+
+//create Profile:
+
+var New = new Profile({name:'Bob', Desired_Country:'India',Local_Country:'Mongolia',Age:'32'});
+
+//adds new profile to profile:
+
+New.save((err,profile)=>{
+    if(err){
+        console.log('error')
+    }
+    else{
+        console.log('saved')
+        console.log(profile)
+    }
+})
+
+//use .create to create and save to database
+
+
+//display Profiles:
+Profile.find({},(err,profiles)=>{
+if(err){
+    console.log('error');
+    console.log(err);
+}
+else{
+    console.log('all profiles')
+    console.log(profiles)
+}
+}
+)
