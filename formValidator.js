0260315bdc5c8a7772a7be3e6aea7dd55f164ac7
SimonTheWang <simonwg2001@gmail.com>
2020-03-26 19:55:22 -0400

    ayt

--- formValidator.js
+++ formValidator.js
@@ -1,6 +1,14 @@
-var element = function(e){
-    document.getElementById(e)
+const socket = io(`http://localhost:3000`)
+
+var element = (e)=>{
+    return document.getElementById(e)
 }
+
+const button = element('submit')
 var username = element('username')
 var password = element('password')
-var email = element('emailnpm')
+var email = element('email')
+
+button.addEventListener('submit',(e)=>{
+e.preventDefault()
+socket.emit('createUser',({username:username,password:password,email:email})
