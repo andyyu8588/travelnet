b6c901828a0fefa935d97fe9bddf68c6eb1b995c
ed99win <edwin99@live.ca>
2020-03-27 18:15:30 -0400

    fini

--- Welcome.js
+++ Welcome.js
@@ -4,5 +4,15 @@ document.getElementById('login').href = og+'login.html'
 document.getElementById('RegistrationPage').href = og+'RegistrationPage.html'
 
 if(document.cookie != []){
-    $("#other").append(`<p>hello ${document.cookie}</p>`)
+    let user
+    let x = JSON.stringify(document.cookie.split(';'))
+    x.split("=")
+    console.log(typeof(x))
+    // array.forEach(element => {
+    //     if(element === 'username'){
+    //         user = x.indexOf(element+1)
+    //     }
+    // });
+
+    $("#other").append(`<p>hello ${user}</p>`)
 }
