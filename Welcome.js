7de9239bdbaeea886fe5dfdb93f4a740573be56d
MoistWhale <andy.yu8588@gmail.com>
2020-03-27 20:25:59 -0400

    cleanup welcome.js

--- Welcome.js
+++ Welcome.js
@@ -1,18 +1,4 @@
 const og = 'http://localhost:3000/'
-document.getElementById('messaging').href = og+'messaging.html'
-document.getElementById('login').href = og+'login.html'
-document.getElementById('RegistrationPage').href = og+'RegistrationPage.html'
-
-if(document.cookie != [] || ''){
-    let user
-    let x = JSON.stringify(document.cookie)
-    x.split(';')
-    if(typeof(x) == "string"){
-        let y = x.split('=')
-        y.forEach(element=>{
-            if(element === '"username' || 'username'){
-            $("#other").append(`<p>hello ${y.indexOf(element+1)}</p>`)
-        }
-    })
-    }
-}
+document.getElementById('messaging').href = og +'messaging.html'
+document.getElementById('login').href = og + 'login.html'
+document.getElementById('RegistrationPage').href = og + 'RegistrationPage.html'
