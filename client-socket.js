9f267bdbea85633efe3e9b6018ae1aab5faf45b8
ed99win <edwin99@live.ca>
2020-03-27 22:10:26 -0400

    thges

--- client-socket.js
+++ client-socket.js
@@ -6,18 +6,25 @@ document.getElementById('tohomepage').href = RegistrationPage
 
 if(document.cookie != ''){
 
+    cookie
     socket.emit('cookie', document.cookie)
     socket.on('cookieres', data=>{
-
+        if(data != {}){
+            cookie = data
+        }
+        else{
+            alert('there was an error processing your demand')
+            document.location.reload()
+        }
     })
 
     // add msg from user to server
     form.addEventListener('submit', (e) => {
     e.preventDefault()
     if (chatmsg.value.length != 0) {
-        socket.emit('CreateChatroom', {user1: ,user2: recipient.value})
-            socket.emit('message', ({msg: chatmsg.value, user: username}))
-            chatmsg.value = ''  
+        socket.emit('CreateChatroom', {user1: cookie.username,user2: recipient.value})
+        socket.emit('message', ({msg: chatmsg.value, user: username}))
+        chatmsg.value = ''  
     }
     })
 
@@ -26,7 +33,6 @@ if(document.cookie != ''){
         $('#chatroom').append(`<li>You: ${chatmsg.value}</li>`)
         $('#chatroom').append(`<li>${data.user}: ${data.msg}</li>`)
     })
-
     }
 
 else{
