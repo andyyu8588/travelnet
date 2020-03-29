9c2a67d29a8097aefb22814b1adb1b19552e42f2
SimonTheWang <simonwg2001@gmail.com>
2020-03-28 22:03:35 -0400

    sefdrgh

--- sever2.js
+++ sever2.js
@@ -1,7 +1,10 @@
-function joinroom(user,room){
+module.exports ={
+    joinroom(user,room) {
     //assign room to client
     socket.join(room)
     //socket.room = room
     console.log(`${user} connected to: ${room}`)
+    }
 }
+
   
