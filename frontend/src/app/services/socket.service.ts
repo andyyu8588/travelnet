7244d53ea03f1fc6469162600b13239497f5b75f
ed99win <edwin99@live.ca>
2020-04-06 23:21:23 -0400

    ok

--- frontend/src/app/services/socket.service.ts
+++ frontend/src/app/services/socket.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
-import * as io from './socket.service';
+import * as io from "socket.io-client/dist/socket.io";
+import { Observable, Subscriber } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
 
-  constructor() { }
-
-  socket: any;
-
-  listen(eventName: string){
-    
+  socket: any
+  readonly uri: string = 'http://localhost:3000'
+  
+  constructor() { 
+    this.socket = io(this.uri)
   }
 
+  listen(eventName: string){
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data) => {
+        subscriber.next(data)
+      })
+    })
+  }
+
+  emit(eventName: string, data: any){
+    this.socket.emit(eventName, data)
+  }
 }
