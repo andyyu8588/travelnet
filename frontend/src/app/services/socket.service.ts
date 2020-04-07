e79cb846c6d8e490a7b2416ef22c6d89edc79972
ed99win <edwin99@live.ca>
2020-04-06 22:13:51 -0400

    include socket

---
+++ frontend/src/app/services/socket.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import * as io from './socket.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+
+  constructor() { }
+}
