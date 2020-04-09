a7375ec144081cbb3e91ecbf160167f0230a4274
ed99win <edwin99@live.ca>
2020-04-08 21:39:09 -0400

    ok

--- frontend/src/app/components/loginpage/loginpage.component.ts
+++ frontend/src/app/components/loginpage/loginpage.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class LoginComponent{
     hideContent = true;
-    private SocketService: SocketService
+    
+
+    constructor(private SocketService: SocketService){
+        this.SocketService.listen('UserIn_res').subscribe((data: any) => {
+            if(data.ans === 'error'){
+                console.log(data.exp)
+            } 
+            else if(data.ans === 'ok'){
+                console.log(data.cookie)
+            }
+        })
+    }
 
     buttonClicked() {
         if (this.hideContent == true){
@@ -21,13 +32,6 @@ export class LoginComponent{
     loginClicked(password, username, event: Event){
         event.preventDefault()
         this.SocketService.emit('UserIn', {email:username , password:password})
-        this.SocketService.listen('UserIn_res').subscribe((data: any) => {
-            if(data.ans === 'error'){
-                console.log(data.exp)
-            } 
-            else if(data.ans === 'ok'){
-                console.log(data.cookie)
-            }
-        }).unsubscribe
+        
     }
 }
