f0bee64d7937786825d29c0882feb2938e4123c3
ed99win <edwin99@live.ca>
2020-04-09 22:44:02 -0400

    ok

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppComponent } from './app.component';
 import {RegistrationComponent} from './components/registrationpage/registrationpage.component';
 import {LoginComponent} from './components/loginpage/loginpage.component';
 import {ChatpageComponent} from './components/chatpage/chatpage.component';
+import { LogoutComponent } from './components/logout/logout.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
     LoginComponent,
     ChatpageComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
