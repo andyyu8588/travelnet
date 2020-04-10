3dbc84a85c29b276aadafcbcad6297e2798129f6
ed99win <edwin99@live.ca>
2020-04-09 22:35:51 -0400

    ok

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {RegistrationComponent} from './components/registrationpage/registrationpage.component';
 import {LoginComponent} from './components/loginpage/loginpage.component';
+import { LogoutComponent } from './components/logout/logout.component';
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
-    LoginComponent
+    LoginComponent,
+    LogoutComponent,
   ],
   imports: [
     BrowserModule,
