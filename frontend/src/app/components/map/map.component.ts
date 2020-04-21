03f25248bcd86501d3ec2f83ab22d8212d25d642
MoistWhale <andy.yu8588@gmail.com>
2020-04-20 21:57:09 -0400

    mapbox pog

--- frontend/src/app/components/map/map.component.ts
+++ frontend/src/app/components/map/map.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from 'src/app/services/session.service';
+import { environment } from 'src/environments/environment';
+
+import * as Mapboxgl from 'mapbox-gl'
 
 @Component({
   selector: 'app-map',
@@ -7,10 +10,26 @@ import { SessionService } from 'src/app/services/session.service';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-session:boolean = this.sessionService.session()
-  constructor(private sessionService:SessionService) { }
 
-  ngOnInit(): void {
+  session: boolean = this.sessionService.session()
+  map: Mapboxgl.Map
+
+  constructor(private sessionService:SessionService) {
+
   }
 
+  ngOnInit(): void {
+
+    Mapboxgl.accessToken = environment.mapbox
+
+    this.map = new Mapboxgl.Map({
+      container: 'mapbox', // container id
+      style: 'mapbox://styles/travelnet/ck999d60d0ntr1jpmyv5losin',
+      center: [-74.5, 40], // starting position
+      zoom: 9 // starting zoom
+    });
+    
+    // Add zoom and rotation controls to the map.
+    this.map.addControl(new Mapboxgl.NavigationControl());
+  }
 }
