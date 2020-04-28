7d211293737bfe3b3c87eed927c1aafa9c337ca0
MoistWhale <andy.yu8588@gmail.com>
2020-04-27 22:12:32 -0400

    ok

--- frontend/src/app/components/friendlist/friend/friend.component.ts
+++ frontend/src/app/components/friendlist/friend/friend.component.ts
@@ -1,3 +1,4 @@
+import { SessionService } from './../../../services/session.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { RoomWidget } from './Room_Widget.model';
 import { FriendlistService } from 'src/app/services/friendlist.service';
@@ -7,15 +8,24 @@ import { FriendlistService } from 'src/app/services/friendlist.service';
   templateUrl: './friend.component.html',
   styleUrls: ['./friend.component.scss']
 })
+
 export class FriendComponent implements OnInit {
 
+  public sessionRoomName: string
+  username: string = sessionStorage.getItem('username')
+
   @Input() friend: RoomWidget
-  constructor(private friendlistService: FriendlistService) {
+  constructor (private friendlistService: FriendlistService,
+    public sessionService: SessionService) {
 
   }
 
   ngOnInit(): void {
-
+    if (this.friend.roomName.replace(`${this.username},`, "").includes(this.friend.roomName)) {
+      this.sessionRoomName = this.friend.roomName.replace(`,${this.username}`, "")
+    } else {
+      this.sessionRoomName = this.friend.roomName.replace(`${this.username},`, "")
+    }
   }
 
   //open or close chat widget 
