19ccedd3f9f6bb6b8c57b61f98d77073befeceed
ed99win <edwin99@live.ca>
2020-04-30 17:58:15 -0400

    ok

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ server.listen(PORT, () => {
 })
 
 // allow static access to folder to get js
-app.use('/', express.static(path.join(__dirname, './frontend/dist/frontend')))
+app.use('/', express.static(path.join(__dirname, 'frontend/dist/frontend')))
 
 // send homepage
 app.use((req,res, next) => {
-  res.sendFile(path.join(__dirname, './frontend/dist/frontend', 'index.html') )
+  res.sendFile(path.join(__dirname, 'frontend/dist/frontend', 'index.html') )
 })
 
 // set URL:
