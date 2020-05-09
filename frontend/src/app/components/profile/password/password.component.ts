5e72378424254c72b29028bcb673b206a6503e95
MoistWhale <andy.yu8588@gmail.com>
2020-05-08 21:59:21 -0400

    ok

---
+++ frontend/src/app/components/profile/password/password.component.ts
@@ -0,0 +1,30 @@
+import { SocketService } from './../../../services/socket.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-password',
+  templateUrl: './password.component.html',
+  styleUrls: ['./password.component.scss']
+})
+export class PasswordComponent implements OnInit {
+  @Input() password: string
+  changeForm: FormGroup
+  changing: boolean
+
+  constructor(private socketService: SocketService) { }
+
+  ngOnInit(): void {
+    this.changeForm = new FormGroup({
+      'newPassword': new FormControl(this.password)
+    })
+  }
+
+  onClick(): void {
+    this.changing = !this.changing
+  }
+
+  onSubmit(): void {
+    this.socketService.emit()
+  }
+}
