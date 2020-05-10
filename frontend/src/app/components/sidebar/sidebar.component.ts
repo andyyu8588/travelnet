42c79bad841ff70e9d66c5f456730d19aee506df
ed99win <edwin99@live.ca>
2020-05-09 20:23:31 -0400

    ok

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  items: Array<any> = ['Home', 'Discover','My Trip']
+  width: number = 30
   constructor() { }
 
   ngOnInit(): void {
