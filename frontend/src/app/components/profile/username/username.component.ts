84a5a041367e168cfb2682d075f1651e46bf19e3
MoistWhale <andy.yu8588@gmail.com>
2020-05-09 21:23:38 -0400

    ok

--- frontend/src/app/components/profile/username/username.component.ts
+++ frontend/src/app/components/profile/username/username.component.ts
@@ -1,3 +1,5 @@
+import { SocketService } from 'src/app/services/socket.service';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
@@ -7,10 +9,33 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class UsernameComponent implements OnInit {
   @Input() username: string
+  changeForm: FormGroup
+  changing: boolean
 
-  constructor() { }
+  constructor(private socketService: SocketService) { }
 
   ngOnInit(): void {
+    this.changeForm = new FormGroup({
+      'newUsername': new FormControl(this.username)
+    })
   }
 
+  // changing between form and h3 with old username
+  onClick(): void {
+    this.changing = !this.changing
+  }
+
+  // once a new username is submitted
+  onSubmit(): void {
+    const requestedChange = {
+      username: this.username,
+      proprety: 'username',
+      newProprety: this.changeForm.get('newUsername').value
+    }
+    this.socketService.emit('editUser', requestedChange, (res) => {
+      console.log(res)
+    })
+    sessionStorage.username = this.changeForm.get('newUsername').value
+    window.location.reload()
+  }
 }
