8caced84d743fab7c17307621249db58f4eade39
ed99win <edwin99@live.ca>
2020-05-09 21:41:30 -0400

    ok

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { FriendlistService } from 'src/app/services/friendlist.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
+  windowSub: Subscription
+  window: boolean
   items: Array<any> = ['Home', 'Discover','My Trip']
   width: number = 30
-  constructor() { }
+
+  constructor(private FriendlistService: FriendlistService) { 
+
+  }
 
   ngOnInit(): void {
+    this.windowSub = this.FriendlistService.windowSize.subscribe((x) => {
+      console.log(x)
+      if (x > 1000) {
+        this.window = true
+      } else {
+        this.window = false
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.windowSub.unsubscribe()
   }
 
 }
