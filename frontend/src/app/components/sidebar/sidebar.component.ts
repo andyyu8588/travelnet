e8458be94f87257856476b2eaeb27e725268a5d6
ed99win <edwin99@live.ca>
2020-05-10 23:11:38 -0400

    ok

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { ResizableModule } from 'angular-resizable-element';
+import { ResizableModule, ResizeEvent } from 'angular-resizable-element';
 import { Subscription } from 'rxjs';
 import { FriendlistService } from 'src/app/services/friendlist.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
@@ -13,6 +13,13 @@ export class SidebarComponent implements OnInit, OnDestroy {
   window: boolean
   items: Array<any> = ['Home', 'Discover','My Trip']
   width: number = 30
+  Styles = {
+    'top': '10%',
+    'bottom': '0%',
+    'left': '2%',
+    'width': '30%'
+  }
+  
 
   constructor(private FriendlistService: FriendlistService,
               private ResizableModule: ResizableModule) { 
@@ -27,6 +34,25 @@ export class SidebarComponent implements OnInit, OnDestroy {
         this.window = false
       }
     })
+  }
+
+  validate(event: ResizeEvent) {
+    const MIN_DIMENSIONS_PX: number = 500;
+    const maxWidth: number = window.innerWidth * 0.96
+    if (
+      event.rectangle.width && // if defined
+        event.rectangle.height && // if defined
+          (event.rectangle.width < MIN_DIMENSIONS_PX ||
+            event.rectangle.width > maxWidth)
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  onResizeEnd(event: any): void {
+    console.log('Element was resized', event);
+    this.Styles.width = `${event.rectangle.width}px`
   }
 
   ngOnDestroy() {
