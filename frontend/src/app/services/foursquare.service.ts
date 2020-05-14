ebbfb57aecc618f7fa044f6cf88d039ef43dd521
ed99win <edwin99@live.ca>
2020-05-13 23:50:38 -0400

    ok

--- frontend/src/app/services/foursquare.service.ts
+++ frontend/src/app/services/foursquare.service.ts
@@ -9,22 +9,30 @@ export class foursquareService {
 
 CLIENT_ID = 'NYZJ324E5GAY2MSQUNYIYLKIDCMX2ETMQREKQXZLW3S5ZYVG';
 CLIENT_SECRET = 'K51P2Y1T3TMTCU24LOFHDFOAONPGU44ZBNZCGTWCOJESUW4A';
-URL = 'https://api.foursquare.com/v2/venues/explore'
+URL = 'https://api.foursquare.com/v2/venues/search'
 lol = 'https://api.foursquare.com/v2/venues/search?client_id=NYZJ324E5GAY2MSQUNYIYLKIDCMX2ETMQREKQXZLW3S5ZYVG&client_secret=K51P2Y1T3TMTCU24LOFHDFOAONPGU44ZBNZCGTWCOJESUW4A&v=20190425&ll=40.7099,-73.9622&intent=checkin&radius=200&query=peter%20luger%20steakhouse'
 
+// headers: new HttpHeaders({
 
-    onSendRequest(){this.http.get<any>(this.lol)
+    // })
+    // {}})
 
-        // headers: new HttpHeaders({
-        // 'client_id': this.CLIENT_ID,
-        // 'client_secret': this.CLIENT_SECRET,
-        // 'll': '40.7243,-74.0018',
-        // 'query': 'coffee',
-        // 'v': '20180323',
-        // })
-        // {}})
-    .subscribe(posts  =>{
-      console.log(posts.response.venues[0]);
-    });
+
+    onSendRequest(){
+      return this.http
+        .get<{[key: string]: any}>(
+          'https://api.foursquare.com/v2/venues/search',
+          {
+            headers: {
+              'client_id': 'NYZJ324E5GAY2MSQUNYIYLKIDCMX2ETMQREKQXZLW3S5ZYVG',
+              'client_secret': 'K51P2Y1T3TMTCU24LOFHDFOAONPGU44ZBNZCGTWCOJESUW4A',
+              'v': '20200513',
+              'll': '40.7099,-73.9622',
+            }
+          }
+        )
+        .subscribe(response => {
+          console.log(response.response)
+        })
   }
 }
