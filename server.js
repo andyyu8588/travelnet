0da47c7e291a8162af13cd7742c0230973b00aae
ed99win <edwin99@live.ca>
2020-05-16 00:18:45 -0400

    ok

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 app.use('/', express.static(path.join(__dirname, 'frontend', 'dist', 'frontend')))
 
 router.get('friends',jwtMiddleware, (req, res, next) => {
-  res.status(200)
+  res.status(201)
   console.log('friends received')
 })
 
