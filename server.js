a374c620e3c66c1a6ed32a4a24d4ec6947ba8a02
ed99win <edwin99@live.ca>
2020-05-16 00:14:53 -0400

    ok

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, authorization"
   )
   res.setHeader("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT")
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'frontend', 'index.html'))
