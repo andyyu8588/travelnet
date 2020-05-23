be77411baed8c69022244bafbf48afe6af65213e
MoistWhale <andy.yu8588@gmail.com>
2020-05-22 21:41:31 -0400

    ok

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -29,17 +29,14 @@ router.post('/edit', (req, res, next) => {
   tempProprety[req.body.params.proprety] = req.body.params.newProprety
   User.findOneAndUpdate({username: req.body.params.username}, {$set: tempProprety}, (err, doc, result) => {        
     if (err) {
-      console.log('a')
       res.status(500).json({
         message: err
       })
     } else if (doc) {
-      console.log('b')
       res.status(200).json({
         message: `Success! ${req.body.params.proprety} changed to ${req.body.params.newProprety}`
       })
     } else {
-      console.log('c')
       res.status(500).json({
         message: 'monkas'
       })
