4828996fa3fea4231bea3ae7a4490b77f4dd239a
SimonTheWang <simonwg2001@gmail.com>
2020-05-22 23:14:07 -0400

    c laiv baby

--- backend/routes/searchusers.js
+++ backend/routes/searchusers.js
@@ -7,29 +7,36 @@ router.get('', (req, res, next) => {
 
     let query = req.query.user
     console.log(query)
-    User.find({$or:
-        [
-                { firstname : { $regex: `.*${query}.*`, $options: 'i'}},
-                { lastname : { $regex: `.*${query}.*`, $options: 'i'}},
-                { username : { $regex: `.*${query}.*`, $options: 'i'}},
-            ],
-            },
-        (err, result) => {
-        if (err) {
-            res.status(500).json({
-                message: err
-              })
-        }
-        else{
-            let resArr = []
-            result.forEach((e)=>{
-            resArr.push(e)
-        })
-        res.status(200).json({
-            users: resArr
-            })
-            }
-        })
-    })
+    User.aggregate([
+        {$project: { "name" : { $concat : [ "$firstname", " ", "$lastname" ] } }},
+        {$match: {"name": {$regex: /.*query.*/i}}}
+      ]).exec(function(err, result){
+        console.log(result);
+      });
 
+    // User.find({$or:
+    //     [
+    //             { firstname : { $regex: `.*${query}.*`, $options: 'i'}},
+    //             { lastname : { $regex: `.*${query}.*`, $options: 'i'}},
+    //             { username : { $regex: `.*${query}.*`, $options: 'i'}},
+    //         ],
+    //         },
+    //     (err, result) => {
+    //     if (err) {
+    //         res.status(500).json({
+    //             message: err
+    //           })
+    //     }
+    //     else{
+    //         let resArr = []
+    //         result.forEach((e)=>{
+    //         resArr.push(e)
+    //     })
+    //     res.status(200).json({
+    //         users: resArr
+    //         })
+    //         }
+    //     })
+    // })
+        })
 module.exports = router
