ed48c8d37e80aef8749b2082b82e4ce7516d721e
SimonTheWang <simonwg2001@gmail.com>
2020-05-22 22:09:59 -0400

    fuck me

--- backend/routes/searchusers.js
+++ backend/routes/searchusers.js
@@ -5,13 +5,13 @@ const User = require("../models/User")
 
 router.get('', (req, res, next) => {
 
-    let query = req.query.user.split(' ')
+    let query = req.query.user
     console.log(query)
     User.find({$or:
         [
-            {'firstname': {$in: query}}, 
-            {'lastname': {$in: query}},
-            {'username': {$in: query}},
+        {'firstname': {$regex: /.*query.*/, $options: 'i'}}, 
+        {'lastname': {$regex: /.*query.*/, $options: 'i'}}, 
+        {'username': {$regex: /.*query.*/, $options: 'i'}}, 
      
         ]},
         (err, result) => {
