905c2423fd3fc418a3cb644d97731b6a8556170f
MoistWhale <andy.yu8588@gmail.com>
2020-05-23 22:04:49 -0400

    ok

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -23,7 +23,7 @@ const storage = multer.diskStorage({
     if (isValid) {
       error = null
     }
-    cb(error, 'backend/images')
+    cb(error, './backend/images')
   },
   filename: (req, file, cb) => {
     const name = file.originalname.toLowerCase().split(' ').join('-')
