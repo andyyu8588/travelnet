43bd585c9881b506cd3694b0881bfb14c37bf3d8
SimonTheWang <simonwg2001@gmail.com>
2020-05-23 21:40:18 -0400

    bruh

--- backend/routes/searchusers.js
+++ backend/routes/searchusers.js
@@ -6,68 +6,41 @@ const User = require("../models/User")
 router.get('', (req, res, next) => {
 
     let query = req.query.user
-    console.log('query is ', query)
-    User.aggregate([
-        {
-          $project: {
-            "name": {
-              $concat: [
-                "$firstname",
-                " ",
-                "$lastname",
-                " ",
-                "$username",
-                " ",
-                "$email",
+    // console.log('query is ', query)
+    // User.aggregate([
+    //     {
+    //       $project: {
+    //         "name": {
+    //           $concat: [
+    //             "$firstname",
+    //             " ",
+    //             "$lastname",
+    //             " ",
+    //             "$username",
+    //             " ",
+    //             "$email",
                 
-              ]
-            },
-            
-          }
-        },
-        {
-          $match: {
-            "name": {
-              $regex: `.*${query}.*`,
-              $options: "i"
-            }
-          }
-        }
-      ])
-      },(err,result) =>{
-        if (err) {
-            res.status(500).json({
-            message: err})
-        }
-        else{
-            console.log(result)
-            let resArr = []
-            result.forEach((e)=>{
-            resArr.push(e)
-        })
-        res.status(200).json({
-            users: resArr
-            })
-            }
-
-      })
-module.exports = router
-
-    // User.find({
-    //     $or:
-    //         [
-    //             { firstname : { $regex: `.*${query}.*`, $options: 'i'}},
-    //             { lastname : { $regex: `.*${query}.*`, $options: 'i'}},
-    //             { username : { $regex: `.*${query}.*`, $options: 'i'}},
-    //         ],
+    //           ]
     //         },
-    //     (err, result) => {
+            
+    //       }
+    //     },
+    //     {
+    //       $match: {
+    //         "name": {
+    //           $regex: `.*${query}.*`,
+    //           $options: "i"
+    //         }
+    //       }
+    //     }
+    //   ])
+    //   },(err,result) =>{
     //     if (err) {
     //         res.status(500).json({
-    //             message: err
-    //           })
+    //         message: err})
     //     }
     //     else{
+    //         console.log(result)
     //         let resArr = []
     //         result.forEach((e)=>{
     //         resArr.push(e)
@@ -76,6 +49,33 @@ module.exports = router
     //         users: resArr
     //         })
     //         }
-    //     })
-   
 
+    //   })
+
+
+    User.find({
+        $or:
+            [
+                { firstname : { $regex: `.*${query}.*`, $options: 'i'}},
+                { lastname : { $regex: `.*${query}.*`, $options: 'i'}},
+                { username : { $regex: `.*${query}.*`, $options: 'i'}},
+            ],
+            },
+        (err, result) => {
+        if (err) {
+            res.status(500).json({
+                message: err
+              })
+        }
+        else{
+            let resArr = []
+            result.forEach((e)=>{
+            resArr.push(e)
+        })
+        res.status(200).json({
+            users: resArr
+            })
+            }
+        })
+})
+module.exports = router
