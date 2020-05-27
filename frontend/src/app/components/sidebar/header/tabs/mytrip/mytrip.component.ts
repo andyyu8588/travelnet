d633dca2c783fc9af6746a91a3d53f7897a995f2
MoistWhale <andy.yu8588@gmail.com>
2020-05-26 22:42:47 -0400

    ok

--- frontend/src/app/components/sidebar/header/tabs/mytrip/mytrip.component.ts
+++ frontend/src/app/components/sidebar/header/tabs/mytrip/mytrip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mytrip',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mytrip.component.scss']
 })
 export class MytripComponent implements OnInit {
+  @Input() name: String = "Sample Trip"
+  @Input() places: Array<any> = []
 
   constructor() { }
 
