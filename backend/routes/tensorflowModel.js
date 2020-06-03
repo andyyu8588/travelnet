81e1f434b0e1a286429f5c7964ae44445402a3dd
ed99win <edwin99@live.ca>
2020-06-03 18:24:40 -0400

    ok

---
+++ backend/routes/tensorflowModel.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const jwtSecret = 'MonkasczI69'
+const jwtMiddleware = require('../jwt.middleware')
+
+const router = express.Router()
+
+router.get('/TfModel', (req, res, next) => {
+    console.log(req)
+    if (req) {
+        res.status(200).sendFile('../Tf/JsModel/model.json')
+    }
+})
+
+module.exports = router
