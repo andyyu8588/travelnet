6eb3d3199b3668d74c1ef4b5801b2bbcd25db3a9
ed99win <edwin99@live.ca>
2020-06-03 18:17:28 -0400

    ok

---
+++ model.js
@@ -0,0 +1,26 @@
+const tf = require('@tensorflow/tfjs')
+const path = require('path')
+
+fileLocation = path.join(__dirname, 'JsModel', 'model.json')
+console.log(fileLocation)
+
+const model = tf.loadLayersModel('https://travelnet.herokuapp.com')
+
+venuesIn = [1, 4, 4, 5]
+
+venuesOut = [3, 3, 2, 1]
+
+model.then((layersModel) => {
+    layersModel.fit(venuesIn, venuesOut, {epochs:300})
+    .then((info) => {
+        console.log('complete')
+        console.log(info)
+    })
+    .catch((err) => {
+        console.log('err')
+        console.log(err)
+    })
+}, (err) => {
+    console.log('rejected')
+    console.log(err)
+})
