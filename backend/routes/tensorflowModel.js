91d4ddd34239ed4f18ccbc5bcd770c1dd3dd4c2e
ed99win <edwin99@live.ca>
2020-06-03 23:03:06 -0400

    ok

--- backend/routes/tensorflowModel.js
+++ backend/routes/tensorflowModel.js
@@ -7,9 +7,9 @@ const path = require('path')
 const router = express.Router()
 
 router.get('/:param', (req, res, next) => {
-    console.log('ok '+req)
-    if (req) {
-        res.status(200).sendFile(req.params, {root: '/app/backend/Tf'})
+    console.log(req.params)
+    if (req.params) {
+        res.status(200).sendFile(req.params.param, {root: path.resolve(__dirname, '..', 'Tf')})
     }
 })
 
--- model.js
+++ model.js
@@ -4,23 +4,27 @@ const path = require('path')
 fileLocation = path.join(__dirname, 'JsModel', 'model.json')
 console.log(fileLocation)
 
-const model = tf.loadLayersModel('https://travelnet.herokuapp.com/TfModel', weightPathPrefix='https://travelnet.herokuapp.com/TfModel')
+const model = tf.loadLayersModel('https://travelnet.herokuapp.com/TfModel/model.json')
 
-venuesIn = [1, 4, 4, 5]
+venuesIn = tf.tensor([1, 4, 4, 5])
 
-venuesOut = [3, 3, 2, 1]
+venuesOut = tf.tensor([3, 3, 2, 1])
 
 model.then((layersModel) => {
+    layersModel.compile({optimizer:tf.train.adam(0.1),
+                        loss:tf.losses.meanSquaredError})
     layersModel.fit(venuesIn, venuesOut, {epochs:300})
-    .then((info) => {
-        console.log('complete')
-        console.log(info)
+    .then((response) => {
+        layersModel.summary()
+        console.log(response)
+        let x = layersModel.predict(tf.tensor([1]))
+        x.print()
     })
     .catch((err) => {
-        console.log('err')
         console.log(err)
     })
 }, (err) => {
     console.log('rejected')
     console.log(err)
 })
+
