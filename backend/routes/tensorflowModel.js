d327f6d01ee938ad23555da3a831416e2420c6e4
ed99win <edwin99@live.ca>
2020-06-03 20:05:42 -0400

    ok

--- backend/routes/tensorflowModel.js
+++ backend/routes/tensorflowModel.js
@@ -9,7 +9,7 @@ const router = express.Router()
 router.get('', (req, res, next) => {
     console.log(req)
     if (req) {
-        res.status(200).sendFile('model.js', {root: '/backend/Tf'})
+        res.status(200).sendFile('model.json', {root: '/backend/Tf'})
     }
 })
 
