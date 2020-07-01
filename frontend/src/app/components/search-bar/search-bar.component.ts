6a35164e7ca56fd8bac0497d6b996e5e487293d6
ed99win <edwin99@live.ca>
2020-07-01 13:01:14 -0400

    ok

--- frontend/src/app/components/search-bar/search-bar.component.ts
+++ frontend/src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2, OnDestroy,ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Renderer2, OnDestroy, ViewChild, ElementRef, AfterViewInit, AfterViewChecked, AfterContentInit, AfterContentChecked } from '@angular/core';
 import { MapService } from 'src/app/services/map/map.service';
 import { Router } from '@angular/router';
 import { SearchService } from 'src/app/services/search.service';
@@ -19,7 +19,7 @@ export class SearchBarComponent implements OnInit, OnDestroy {
   private returnTab: Subscription
   @ViewChild('searchResultsContainer') div: ElementRef
 
-  select: number = 0
+  defaultFilter: any = 'All'
 
   constructor(
     private map: MapService,
@@ -27,39 +27,39 @@ export class SearchBarComponent implements OnInit, OnDestroy {
     private router : Router,
     private SearchService: SearchService
   ) {
-
   }
 
   ngOnInit(): void {
     this.returnTab = this.SearchService.searchTab.subscribe((tab)=> this.openTab = tab)
   }
 
+
   onSubmit(data: string) {
     this.SearchService.enterSearch(data,this.SearchService.mainSearch(data, this.map.getCenter())).then(()=>{
     this.router.navigate([this.openTab.path])
     })
   }
+
   changeFilter(filter){
     this.SearchService.changeFilter(parseInt(filter.value))
   }
 
-
   onKey(data: string) {
     if (data === "") {
-        this.loading = false
+      this.loading = false
     } else {
-        this.loading = true
-        this.SearchService.mainSearch(data, this.map.getCenter())
-        .then((finalData) => {
-            this.loading = false
-            this.Renderer.removeChild(this.div, this.child)
-            this.child = this.Renderer.createElement('p');
-            this.child.innerHTML = finalData[1]
-            this.Renderer.appendChild(this.div.nativeElement, this.child)
-        })
-        .catch((err) => {
-            this.loading = false
-        })
+      this.loading = true
+      this.SearchService.mainSearch(data, this.map.getCenter())
+      .then((finalData) => {
+        this.loading = false
+        this.Renderer.removeChild(this.div, this.child)
+        this.child = this.Renderer.createElement('p');
+        this.child.innerHTML = finalData[1]
+        this.Renderer.appendChild(this.div.nativeElement, this.child)
+      })
+      .catch((err) => {
+        this.loading = false
+      })
     }
   }
 
