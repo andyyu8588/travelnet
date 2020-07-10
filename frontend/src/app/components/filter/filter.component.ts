ad5d948ca00b7e3479a17e785b8b1879405748c6
SimonTheWang <simonwg2001@gmail.com>
2020-07-10 09:29:06 +0900

    childeren checked error

--- frontend/src/app/components/filter/filter.component.ts
+++ frontend/src/app/components/filter/filter.component.ts
@@ -29,11 +29,6 @@ export class FilterComponent implements OnInit {
   ngOnInit(): void {
     this.SearchService.updateCategories()
     .then(x => {
-      // x.map(d => {
-      //   d['checked'] = true;
-      //   d['expanded'] = false;
-      //   return d;
-      // })
       this.categories = x
       console.log(this.categories)
 
@@ -50,62 +45,72 @@ export class FilterComponent implements OnInit {
 
   /**toggle clicks checkmarks */
   clickedActive(element) {
+    console.log(this.categories)
     element.checked = !element.checked;
   }
   /**makes all children nodes of a parent node checked when checked, and the opposite if need be */
   setAll(category) {
+    // console.log(category)
     category.checked = !category.checked;
     if(category.checked) {
-      this.checkAll(category)
+      this.checkAll(category.categories)
     }
     else if (!category.checked){
-      this.uncheckAll(category)
+      this.uncheckAll(category.categories)
     }
   }
   /** checks all children of a node */
-  checkAll(category){
-    if(category.categories){
-      category.categories.forEach(sub => {
+  checkAll(categories){
+      categories.forEach(sub => {
       sub.checked = true;
       if(sub.categories && sub.categories.length > 0){
-          this.checkAll(sub.categories)
-        }
-      });
-    }
+        sub.checked = true;
+        this.checkAll(sub.categories)
+      }
+  });
 
   }
   /** unchecks all children of a node */
-  uncheckAll(category){
-    if(category.categories){
-      category.categories.forEach(sub => {
+  uncheckAll(categories){
+      categories.forEach(sub => {
         sub.checked = false;
         if(sub.categories && sub.categories.length > 0){
+          sub.checked = false;
           this.uncheckAll(sub.categories)
         }
-      });
-    }
+    });
   }
 
   /** returns false if node has unchecked children, and true if all children are checked */
-  childrenChecked(node: CategoryNode): boolean {
-    // console.log(node)
-    if (node.categories) {
-      node.categories.forEach((node: CategoryNode) => {
+  childrenChecked(categories: CategoryNode[]): boolean {
+    // if (node.categories) {
+      categories.forEach((node: CategoryNode) => {
         if (node.checked) {
-          if (node.categories) {
-            this.childrenChecked(node)
+          if (node.categories && node.categories.length > 0) {
+            this.childrenChecked(node.categories)
+          }
+          else{
+            if (node.checked) {
+            } else {
+              return false
+            }
           }
         } else {
           return false
         }
       })
-    } else {
-      if (node.checked) {
-        return true
-      } else {
-        return false
-      }
-    }
+      return true
+    // }
+    //if it is a leaf
+    //problem: returns value too quickly
+    // else {
+    //   if (node.checked) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
   }
+
 
 }
