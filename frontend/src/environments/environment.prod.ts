1810f7920dae32baeb979c272c6cd9e09d89ed46
andyyu8588 <andy.yu8588@gmail.com>
2020-12-25 20:53:30 -0500

    change server to local

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
 
   production: true,
   language: 'en',
-  travelnetURL: 'https://travelnet.herokuapp.com',
+  travelnetURL: 'localhost:3000',
 
   mapbox: {
     token: 'pk.eyJ1IjoidHJhdmVsbmV0IiwiYSI6ImNrOTk3cHkwaDAzaHkzZHEwMm03ZGN0MG8ifQ.j24u0Q5RbYw7PW4tVpGjmQ',
